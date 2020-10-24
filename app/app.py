b16dfaba7e6fa85a7918f9f06df829a08ff3dde8
Garrit Schroeder <garritsidney.schroeder@ottogroup.com>
2020-10-25 00:23:57 +0200

    implement cache for azure call

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from flask import Flask
 from flask import render_template
@@ -7,6 +7,27 @@ from pip._vendor import requests
 
 template_dir = os.path.abspath('templates')
 app = Flask(__name__)
+AZURE_RESPONSE = None
+
+
+def get_azure_information():
+    global AZURE_RESPONSE
+    if AZURE_RESPONSE is None or AZURE_RESPONSE['date'] < datetime.utcnow() + timedelta(minutes=-5):
+        today = datetime.now()
+        json = {
+            "calendar_filter_start": today.strftime("%Y-%m-%dT%H:%M"),
+            "calendar_filter_end": today.strftime("%Y-%m-%dT23:59")
+        }
+        res = requests.post(os.environ.get("LOGIC_APP_URL"), json=json)
+        AZURE_RESPONSE = {
+            "date": datetime.utcnow(),
+            "data": res.json(),
+            "cached": False
+        }
+        return AZURE_RESPONSE
+    else:
+        AZURE_RESPONSE['cached'] = True
+        return AZURE_RESPONSE
 
 
 @app.template_filter('strftime')
@@ -19,18 +40,9 @@ def format_datetime(value):
 def hello_world():
     if os.environ.get("LOGIC_APP_URL") is None:
         return "environment variable LOGIC_APP_URL is not set"
-
-    today = datetime.now()
-    json = {
-        "calendar_filter_start": today.strftime("%Y-%m-%dT%H:%M"),
-        "calendar_filter_end": today.strftime("%Y-%m-%dT23:59")
-    }
-    res = requests.post(os.environ.get("LOGIC_APP_URL"), json=json)
-    data = res.json()
-    if res.status_code == 200:
-        return render_template('index.html', data=res.json())
-    else:
-        return "error calling azure logic app"
+    data = get_azure_information()
+    print("cached = " + str(data['cached']))
+    return render_template('index.html', data=data['data'])
 
 
 if __name__ == "__main__":
